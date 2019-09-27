21398ef23d7b7870697b2b6950ee86a63c961908
Finn <10346401+finncodes@users.noreply.github.com>
2019-09-27 22:49:05 +1200

    Added the ability to find Onzo bikes

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const fetch = require('node-fetch');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/onzo/:lat/:lon/:range', async function (req, res) {
+    const apiResponse = await fetch(`https://app.onzo.co.nz/nearby/${req.params.lat}/${req.params.lon}/5000.0.json`,
+        {
+            headers: {
+                "Accept": "application/json, text/plain, */*",
+                "Origin": "https://app.onzo.co.nz",
+                "Referer": "https://app.onzo.co.nz/nearby",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
+            }
+        });
+    return res.json(await apiResponse.json()).end();
+});
 
 module.exports = app;
